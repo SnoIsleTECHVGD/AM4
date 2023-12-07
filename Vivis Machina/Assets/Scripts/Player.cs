3720c2f4761bb5e525c1049c913ece7b7b000946
Burgos <1718014@mukilteo.wednet.edu>
2023-12-07 09:29:27 -0800

    push

--- Vivis Machina/Assets/Scripts/Player.cs
+++ Vivis Machina/Assets/Scripts/Player.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int speed;
+    public float speed;
+
+    bool inAir;
 
     public Rigidbody2D rb;
     public SpriteRenderer sr;
 
     void Start()
     {
-        
+
     }
-    
+
     void Update()
     {
         if (Input.GetKey(KeyCode.RightArrow))
@@ -30,5 +32,21 @@ public class Player : MonoBehaviour
         {
             rb.velocity = new Vector3(0, rb.velocity.y, 0);
         }
+        if (Input.GetKey(KeyCode.X) && !inAir)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 2, 0);
+        }
+        if (true)
+        {
+            inAir = false;
+        }
+        else
+        {
+            inAir = true;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.contacts);
     }
 }
