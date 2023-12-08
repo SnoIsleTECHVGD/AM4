aeb589647b15dc8f29e79c58c5fcc7bcf43f49b0
Burgos <1718014@mukilteo.wednet.edu>
2023-12-08 09:30:29 -0800

    movement work

--- Vivis Machina/Assets/Scripts/Level Scripts/Player.cs
+++ Vivis Machina/Assets/Scripts/Level Scripts/Player.cs
@@ -5,48 +5,73 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
+    public float ease;
 
     bool inAir;
+    Vector2 colPoint;
+    bool[] onWall = new bool[] { false, false };
+    float xVelocity;
 
     public Rigidbody2D rb;
     public SpriteRenderer sr;
 
-    void Start()
-    {
-
-    }
-
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && !onWall[1])
         {
-            rb.velocity = new Vector3(speed, rb.velocity.y, 0);
+            xVelocity = Mathf.Min(xVelocity + Time.deltaTime * ease, speed);
             sr.flipX = false;
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow) && !onWall[0])
         {
-            rb.velocity = new Vector3(-speed, rb.velocity.y, 0);
+            xVelocity = Mathf.Max(xVelocity - Time.deltaTime * ease, -speed);
             sr.flipX = true;
+        }
+        else if (Mathf.Abs(xVelocity) > 0.1f)
+        {
+            xVelocity -= Mathf.Sign(xVelocity) * Time.deltaTime * ease;
         }
         else
         {
-            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+            xVelocity = 0;
         }
-        if (Input.GetKey(KeyCode.X) && !inAir)
+        if (Input.GetKeyDown(KeyCode.X) && !inAir)
         {
-            rb.velocity = new Vector3(rb.velocity.x, 2, 0);
+            inAir = true;
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, 0);
         }
-        if (true)
+        else if (Input.GetKeyDown(KeyCode.X) && onWall[0])
+        {
+            rb.velocity = new Vector3(speed, jumpForce, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.X) && onWall[1])
+        {
+            rb.velocity = new Vector3(-speed, jumpForce, 0);
+        }
+        rb.velocity = new Vector3(xVelocity, rb.velocity.y, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        colPoint = collision.contacts[0].normal;
+        if (colPoint == new Vector2(0, 1))
         {
             inAir = false;
         }
-        else
+        else if (Mathf.Abs(colPoint.x) == 1)
         {
-            inAir = true;
+            StartCoroutine(HitWall(Mathf.RoundToInt(-colPoint.x / 2 + 0.5f)));
         }
     }
-    void OnCollisionEnter2D(Collision2D collision)
+
+    IEnumerator HitWall(int side)
     {
-        Debug.Log(collision.contacts[0].point);
+        onWall[side] = true;
+        while (Mathf.Abs(colPoint.x) == 1)
+        {
+            yield return 1;
+        }
+        onWall[side] = false;
     }
 }
