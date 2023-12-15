41b44e29483f496ebe722fd96dcd89e41dc87f93
Burgos <1718014@mukilteo.wednet.edu>
2023-12-15 09:33:30 -0800

    bad parallax

--- Vivis Machina/Assets/Scripts/Level Scripts/Parallax.cs
+++ Vivis Machina/Assets/Scripts/Level Scripts/Parallax.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     public Vector2 parallaxScale;
-    public float sizeX;
-    public int num;
+    public SpriteRenderer sr;
+
+    float size;
 
     void Start()
     {
-        
+        size = sr.sprite.rect.size.x / 100f;
+        Debug.Log(size);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(CameraMove.pos.x * parallaxScale.x - sizeX / 2 + sizeX * num, CameraMove.pos.y * parallaxScale.y);
+        transform.position = new Vector2(CameraMove.pos.x * parallaxScale.x - Mathf.RoundToInt(CameraMove.pos.x * parallaxScale.x / size) * -size, CameraMove.pos.y * parallaxScale.y);
+        Debug.Log(Mathf.RoundToInt(CameraMove.pos.x / size / size) * -size);
     }
 }
